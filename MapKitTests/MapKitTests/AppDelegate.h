a2fa898366eb79592c1d87cec94382b78c4d5a0d
wer <23432532@qq.com>
2015-11-03 19:29:21 +0800

    初始化项目

---
+++ MapKitTests/MapKitTests/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapKitTests
+//
+//  Created by 杨振 on 15/11/3.
+//  Copyright © 2015年 杨振. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MapKitTests/MapKitTests/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MapKitTests
+//
+//  Created by 杨振 on 15/11/3.
+//  Copyright © 2015年 杨振. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
